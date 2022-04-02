92157878ce7487c20c0b1ba11d37cc73dff0a452
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-04-02 21:42:48 +0800

    Add markdown preview plugin & syntax

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -32,6 +32,14 @@ require('packer').startup(function()
   -- whitespace detector
   use 'ntpeters/vim-better-whitespace'
 
+  -- markdown preview
+  use {
+    'iamcco/markdown-preview.nvim',
+    run = 'cd app && npm install',
+    setup = function() vim.g.mkdp_filetypes = { 'markdown' } end,
+    ft = { 'markdown' }
+  }
+
   -- markdown checkbox
   use 'jkramer/vim-checkbox'
 
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -119,6 +119,13 @@ _G.packer_plugins = {
     path = "/home/bandithijo/.local/share/nvim/site/pack/packer/start/lexima.vim",
     url = "https://github.com/cohama/lexima.vim"
   },
+  ["markdown-preview.nvim"] = {
+    loaded = false,
+    needs_bufread = false,
+    only_cond = false,
+    path = "/home/bandithijo/.local/share/nvim/site/pack/packer/opt/markdown-preview.nvim",
+    url = "https://github.com/iamcco/markdown-preview.nvim"
+  },
   neoformat = {
     loaded = true,
     path = "/home/bandithijo/.local/share/nvim/site/pack/packer/start/neoformat",
@@ -257,6 +264,17 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Setup for: markdown-preview.nvim
+time([[Setup for markdown-preview.nvim]], true)
+try_loadstring("\27LJ\2\n=\0\0\2\0\4\0\0056\0\0\0009\0\1\0005\1\3\0=\1\2\0K\0\1\0\1\2\0\0\rmarkdown\19mkdp_filetypes\6g\bvim\0", "setup", "markdown-preview.nvim")
+time([[Setup for markdown-preview.nvim]], false)
+vim.cmd [[augroup packer_load_aucmds]]
+vim.cmd [[au!]]
+  -- Filetype lazy-loads
+time([[Defining lazy-load filetype autocommands]], true)
+vim.cmd [[au FileType markdown ++once lua require("packer.load")({'markdown-preview.nvim'}, { ft = "markdown" }, _G.packer_plugins)]]
+time([[Defining lazy-load filetype autocommands]], false)
+vim.cmd("augroup END")
 if should_profile then save_profiles() end
 
 end)
