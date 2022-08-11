49857988771c6d314cd72372340e12335229eafc
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-08-11 08:22:14 +0800

    Add plugin: lspkin.nvim for iocn on nvim-cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -145,6 +145,7 @@ require('packer').startup(function()
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
+  use 'onsails/lspkind.nvim'
 
   -- snippet
   use 'hrsh7th/cmp-vsnip'
--- lua/user/nvim-cmp.lua
+++ lua/user/nvim-cmp.lua
@@ -71,3 +71,20 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 require('lspconfig')['sumneko_lua'].setup {
   capabilities = capabilities
 }
+
+-- luakind.nvim config
+local lspkind = require('lspkind')
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
+        return vim_item
+      end
+    })
+  }
+}
