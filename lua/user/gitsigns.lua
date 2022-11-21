3d78e56d6b33a72be12630a5847dcfe995006f57
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-11-21 14:40:28 +0800

    gitsigns: add untracked gitsigns text

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -5,6 +5,7 @@ require('gitsigns').setup {
     changedelete = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
     delete       = {hl = 'GitSignsDelete', text = '│', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
     topdelete    = {hl = 'GitSignsDelete', text = '│', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    untracked    = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
   },
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
   numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
