01fdcfad416cd9f72d3c71282460247b4c4a5da1
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2023-06-28 22:40:38 +0800

    LSP init.lua: Add ejs on filetypes

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -59,6 +59,6 @@ require("lspconfig").cssls.setup({
 require("lspconfig").emmet_ls.setup({
   on_attach = on_attach,
   filetypes = {
-    "css", "eruby", "html", "javascript", "javascriptreact", "less", "liquid", "sass", "scss", "svelte", "pug", "typescriptreact", "vue"
+    "css", "ejs", "eruby", "html", "javascript", "javascriptreact", "less", "liquid", "sass", "scss", "svelte", "pug", "typescriptreact", "vue"
   }
 })
