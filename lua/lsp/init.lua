8f3e76675efd0787e3e7fe6ec5bf3d4b25eae3a8
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2023-08-07 23:12:39 +0800

    chore: remove nvim-navic local on_attach, change into lspconfig

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,8 +1,6 @@
-local navic = require("nvim-navic")
-local on_attach = function(client, bufnr)
-  if client.server_capabilities.documentSymbolProvider then
-    navic.attach(client, bufnr)
-  end
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
 end
 
 require("lspconfig").lua_ls.setup({
