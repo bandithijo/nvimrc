3845d002cfd18975d8f39efcb4173ab6ea71349d
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2023-08-09 01:38:10 +0800

    feat: add custom statusline

---
+++ lua/user/statusline.lua
@@ -0,0 +1,20 @@
+local function statusline()
+  local set_color = "%#StatusLine#"
+  local filename = "%<%{expand('%:.') != '' ? expand('%:.') : '[No Name]'}"
+  local modified = "%w%q%h%r%m"
+  local align_r = "%="
+  local linecol = "%-14.(%l,%c%V%)"
+  local percentage = "%P"
+
+  return string.format(
+    "%s%s %s %s %s %s",
+    set_color,
+    filename,
+    modified,
+    align_r,
+    linecol,
+    percentage
+  )
+end
+
+vim.opt.statusline = statusline()
