d10728fe442c8bab41b46ef9bbe3b758ac96af56
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2023-09-10 22:24:21 +0800

    chore: update statusline
    Add state active and inactive

--- lua/user/statusline.lua
+++ lua/user/statusline.lua
@@ -1,9 +1,11 @@
-local function statusline()
+Statusline = {}
+
+Statusline.active = function()
   local set_color = "%#StatusLine#"
   local filename = "%<%{expand('%:.') != '' ? expand('%:.') : '[No Name]'}"
   local modified = "%w%q%h%r%m%y"
   local align_r = "%="
-  local linecol = "%-14.(%l,%c%V%)"
+  local linecol = "%-5.(%l,%c%V%)"
   local percentage = "%P"
 
   return string.format(
@@ -17,4 +19,27 @@ local function statusline()
   )
 end
 
-vim.opt.statusline = statusline()
+Statusline.inactive = function()
+  local set_color = "%#StatusLine#"
+  local filename = "%<%{expand('%:.') != '' ? expand('%:.') : '[No Name]'}"
+  local modified = "%w%q%h%r%m%y"
+  local align_r = "%="
+  local percentage = "%P"
+
+  return string.format(
+    "%s%s %s %s %s",
+    set_color,
+    filename,
+    modified,
+    align_r,
+    percentage
+  )
+end
+
+vim.api.nvim_exec([[
+  augroup statusline
+  au!
+  au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline.active()
+  au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline.inactive()
+  augroup END
+]], false)
