5b3ead73f04fcd91b1a768eb8fa57b724e60faa5
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2023-11-09 22:37:42 +0800

    disable: auto update trigger on buffer change or terminal focus

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -12,7 +12,7 @@ autocmd TermOpen * setlocal nonumber norelativenumber scl=no
 autocmd CursorHold,CursorHoldI * checktime
 
 " auto update trigger on buffer change or terminal focus
-autocmd FocusGained,BufEnter * checktime
+" autocmd FocusGained,BufEnter * checktime
 
 " toggle for markdown checkbox with <leader>tt remap to <leader>cc
 autocmd FileType markdown,pango,asc map <silent> <Leader>cc :call checkbox#ToggleCB()<CR>
