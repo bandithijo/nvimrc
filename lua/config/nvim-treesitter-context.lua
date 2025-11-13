8f77dc01591d13131d285fd3d1b3877bd35852c1
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-11-13 20:18:17 +0800

    chore: Change multiline_threshold on nvim-treesitter-context

--- lua/config/nvim-treesitter-context.lua
+++ lua/config/nvim-treesitter-context.lua
@@ -8,7 +8,7 @@ require("treesitter-context").setup({
   max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
   min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
   line_numbers = true,
-  multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+  multiline_threshold = 5, -- Maximum number of lines to collapse for a single context line
   trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
   mode = "cursor",  -- Line used to calculate context. Choices: 'cursor', 'topline'
   -- Separator between context and content. Should be a single character string, like '-'.
